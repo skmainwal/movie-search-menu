b5f2b6dc78c340108bfc13098a5788c85dd10b3e
Shubham Kumar <shubhamsre1997@gmail.com>
2020-09-16 11:48:37 +0530

    testing poster

--- src/App.js
+++ src/App.js
@@ -10,14 +10,9 @@ function App() {
         <input type="text" />
       </div>
       <div className="movies_container">
-        <div className="movie_contaier">
+        <div className="movie_container">
           <div className="movie_image"></div>
           <p className="movie-title">Aquaman</p>
-          <p className="movie-year">2005</p>
-        </div>
-        <div className="movie_contaier">
-          <div className="movie_image"></div>
-          <p className="movie-title">Dark knight</p>
           <p className="movie-year">2005</p>
         </div>
       </div>
