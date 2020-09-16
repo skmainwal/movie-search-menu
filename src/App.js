91ce07691b5b9f4bd7d6bdba492ffd799410a545
Shubham Kumar <shubhamsre1997@gmail.com>
2020-09-16 11:30:15 +0530

    adding photos/css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "../src/style/main.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="container">
+      <div className="input_continare">
+        <label>Search for a Movie</label>
+
+        <input type="text" />
+      </div>
+      <div className="movies_container">
+        <div className="movie_contaier">
+          <div className="movie_image"></div>
+          <p className="movie-title">Aquaman</p>
+          <p className="movie-year">2005</p>
+        </div>
+        <div className="movie_contaier">
+          <div className="movie_image"></div>
+          <p className="movie-title">Dark knight</p>
+          <p className="movie-year">2005</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
